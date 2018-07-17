7873e40f14dcd36b92a4543f8b447be89be72e97
Mark Pashby <markpashby@gmail.com>
2018-07-17 17:49:32 +0100

    Removed scripts directive and changed package management function as well as change to project entry points

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from ecsopera.version import __version__
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='ecsopera',
       version=__version__,
@@ -7,7 +7,23 @@ setup(name='ecsopera',
       url='http://github.com/Pashbee/ecsopera',
       author='Pashbee',
       license='MIT',
-      packages=['ecsopera'],
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
       install_requires=['click', 'boto3', 'moto', 'progressbar2', 'pytest'],
-      scripts=['bin/ecsopera'],
-      zip_safe=False)
+      zip_safe=False,
+      entry_points={
+        'console_scripts': [
+            'ecsopera = ecsopera.cli:ecsopera',
+        ],
+      },
+      classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+])
